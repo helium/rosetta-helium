b3f21b9fd6e538916f63d77793564768e9ef2386
Steven Yuan <syuan100@gmail.com>
2021-04-27 12:51:54 -0700

    Add payment_v2 support

--- helium/transactions.go
+++ helium/transactions.go
@@ -35,6 +35,28 @@ func OperationsFromTx(txn map[string]interface{}) ([]*types.Operation, *types.Er
 		return RewardV1(
 			fmt.Sprint(txn["payee"]),
 			utils.MapToInt64(txn["amount"]))
+	case PaymentV2Txn:
+		feeDetails, bErr := GetImplicitBurn(fmt.Sprint(txn["hash"]))
+		if bErr != nil {
+			feeDetails = &Fee{
+				Amount:   utils.MapToInt64(txn["fee"]),
+				Payer:    fmt.Sprint(txn["payer"]),
+				Currency: DC,
+			}
+		}
+		var payments []*Payment
+		for _, p := range txn["payments"].([]map[string]interface{}) {
+			payments = append(payments, &Payment{
+				Payee:  fmt.Sprint(p["payee"]),
+				Amount: utils.MapToInt64(p["amount"]),
+			})
+		}
+		return PaymentV2(
+			fmt.Sprint(txn["payer"]),
+			payments,
+			feeDetails.Amount,
+			feeDetails.Currency.Symbol,
+		)
 	default:
 		return nil, WrapErr(ErrNotFound, errors.New("txn type not found"))
 	}
@@ -62,6 +84,35 @@ func PaymentV1(payer string, payee string, amount int64, fee int64, feeType stri
 		PaymentCredit,
 		Fee,
 	}, nil
+
+}
+
+func PaymentV2(payer string, payments []*Payment, fee int64, feeType string) ([]*types.Operation, *types.Error) {
+
+	var paymentV2Operations []*types.Operation
+
+	for i, p := range payments {
+		PaymentDebit, pErr := CreatePaymentDebitOp(&p.Payee, &p.Amount, int64(2*i))
+		if pErr != nil {
+			return nil, pErr
+		}
+
+		PaymentCredit, pcErr := CreatePaymentCreditOp(&p.Payee, &p.Amount, int64((2*i)+1))
+		if pcErr != nil {
+			return nil, pcErr
+		}
+
+		paymentV2Operations = append(paymentV2Operations, PaymentDebit, PaymentCredit)
+	}
+
+	Fee, fErr := CreateFeeOp(&payer, &fee, &feeType, int64(len(paymentV2Operations)))
+	if fErr != nil {
+		return nil, fErr
+	}
+
+	paymentV2Operations = append(paymentV2Operations, Fee)
+
+	return paymentV2Operations, nil
 
 }
 
