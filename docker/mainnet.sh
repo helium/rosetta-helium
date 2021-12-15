e05503ce46081aa287b2061e751566b15db301a3
Ubuntu <ubuntu@ip-172-31-50-155.us-west-2.compute.internal>
2021-12-15 06:39:32 +0000

    Add missing lbs file for mainnet

--- docker/mainnet.sh
+++ docker/mainnet.sh
@@ -2,6 +2,7 @@
 
 set -euo pipefail
 
+cat /opt/blockchain_node/config/sys.config | grep -oP '(?<=\{blessed_snapshot_block_height\, ).*?(?=\})' > /app/lbs.txt &
 tail -n 0 -q -F /opt/blockchain_node/log/*.log* >> /proc/1/fd/1 &
 
 /opt/blockchain_node/bin/blockchain_node daemon
